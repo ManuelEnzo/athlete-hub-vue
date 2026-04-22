5c1fd84fd108695df1a61ff5cf07b57323c484c8
ManuelEnzo <manuelenzo2000@gmail.com>
2026-04-22 21:23:51 +0200

    Test vercel

--- athlete-hub/nuxt.config.ts
+++ athlete-hub/nuxt.config.ts
@@ -85,7 +85,7 @@ export default defineNuxtConfig({
 
   compatibilityDate: '2024-12-14',
   nitro: {
-    preset: 'vercel-static',
+    preset: 'static',
     routeRules: {
       '/**': {
         headers: {
